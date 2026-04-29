89f22a9cc7e528067014615c39f22c1a2b365811
shikhartpl <karan731373@gmail.com>
2026-04-29 12:08:16 +0530

    events changes

--- our-events.php
+++ our-events.php
@@ -13,7 +13,24 @@
 <?php include 'includes/breadcrumb.php'; ?> 
 <section class="new-content py-5 my-5">
     <div class="container">
-        <div class="row">
+           <div class="row">
+            
+            <div class="col-12 col-md-6 col-lg-5">
+                <div class="events-img">
+                    <img src="assets/images/events-2.png" alt="Events Image">
+                    
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-7 d-flex align-items-center justify-content-center">
+                <div class="events-content">
+                    <h2><span>23 April 2025</span></h2>
+                    <h3>International Exhibitions — Chinaplas</h3>
+                    <p>We recently visited one of the world’s leading international exhibitions — Chinaplas — to explore cutting-edge innovations, build global connections, and bring back fresh ideas to shape the future of packaging. 💡🌐</p>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-4">
+            
             <div class="col-12 col-md-6 col-lg-5">
                 <div class="events-img">
                     <img src="assets/images/events.png" alt="Events Image">
@@ -25,6 +42,22 @@
                     <h2><span>23 April 2025</span></h2>
                     <h3>International Exhibitions — Chinaplas</h3>
                     <p>We recently visited one of the world’s leading international exhibitions — Chinaplas — to explore cutting-edge innovations, build global connections, and bring back fresh ideas to shape the future of packaging. 💡🌐</p>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-12 col-md-6 col-lg-5">
+                <div class="events-img">
+                    <img src="assets/images/events-1.png" alt="Events Image">
+                    
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-7 d-flex align-items-center justify-content-center">
+                <div class="events-content">
+                    <h2><span>10 February 2026</span></h2>
+                    <h3>International Exhibitions — PlastIndia 2026, New Delhi</h3>
+                    <p>We recently participated in PlastIndia 2026 at Bharat Mandapam, New Delhi — one of the world’s leading exhibitions for plastics and packaging. The event gave us a valuable opportunity to showcase our packaging innovations, connect with industry professionals, and explore emerging trends shaping the future of packaging. 🌍✨
+ </p>
                 </div>
             </div>
         </div>
